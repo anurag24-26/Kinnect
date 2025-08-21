30774832655a45bd71706de0dfba47fa975b5df0
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2025-08-21 09:39:00 +0530

    frontend chats Resolved

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -13,7 +13,7 @@ router.get("/:user1/:user2", async (req, res) => {
         { senderId: user1, receiverId: user2 },
         { senderId: user2, receiverId: user1 },
       ],
-    }).sort({ createdAt: -1 });
+    }).sort({ createdAt: 1 });
 
     res.json(messages);
   } catch (err) {
